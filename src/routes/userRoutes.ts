5979c68d9d28e3e8d3ec35c23155d45fbf973d35
iagod <iago.tostes@cubos.io>
2022-07-09 23:35:47 -0300

    chore: changes on user validation schema

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 import * as userController from '../controllers/userController';
-import { validateBody } from '../middlewares/schemaValidation'
-import { userSchema } from '../schemas/userSchema'
+import { validateBody, validateParams } from '../middlewares/schemaValidation'
+import { createUserSchema, updateUserBodySchema, UserParamsSchema } from '../schemas/userSchema'
 
 const router = express.Router();
 
-router.post("/users", validateBody(userSchema), userController.create);
-router.get("/users", userController.findAll);
-router.get("/users/:id", userController.findOne);
-router.put("/users/:id", userController.update);
+router.post("/users", 
+    validateBody(createUserSchema), 
+    userController.create
+);
+
+router.get("/users", 
+    userController.findAll
+);
+
+router.get("/users/:id",
+    validateParams(UserParamsSchema),  
+    userController.findOne
+);
+
+router.put("/users/:id", 
+    validateBody(updateUserBodySchema), 
+    validateParams(UserParamsSchema), 
+    userController.update
+);
 
 export default router;
--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,8 +1,18 @@
 import joi from 'joi'
 import { UserParams } from '../types/userTypes.js'
 
-export const userSchema = joi.object<UserParams>({
+export const createUserSchema = joi.object<UserParams>({
 	name: joi.string().required(),
 	password: joi.string().min(6).required(),
 	email: joi.string().required()
+})
+
+export const updateUserBodySchema = joi.object<Partial<UserParams>>({
+	name: joi.string(),
+	password: joi.string().min(6),
+	email: joi.string()
+})
+
+export const UserParamsSchema = joi.object<{ id: number }>({
+	id: joi.number().required()
 })
